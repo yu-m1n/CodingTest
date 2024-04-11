7be485fe763967c894333873f068eb2cadd379eb
yumin <rdddq01@gmail.com>
2024-04-11 18:03:40 +0900

    Basic commit

---
+++ beakjun/java10998.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class java10998 {
+    Scanner sc = new Scanner(System.in);
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+
+    System.out.println(num1 * num2);
+}
--- beakjun/java2750.java
+++ beakjun/java2750.java
@@ -1,6 +1,34 @@
 import java.util.*;
-import java.util.Arrays;
 public class java2750 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); //Scanner 값을 int로(정수)
+        int[] arr = new int[N]; //배열 생성
+
+        for(int i = 0; i < N; i++){ //N으로 입력받은 arr 배열만큼 정수 입력 받음
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < N; i++){
+            int temp =0; // 상대적인 값 비교를 위한 변수 생성
+            for(int j = 0; j < N; j++){
+                if(arr[j] > arr[i]){
+                    temp = arr[j]; //temp에 arr 첫번째 값을 넣음
+                    arr[j] = arr[i]; //arr 첫번째 값과 두 번째 값 비교해 정렬
+                    arr[i] = temp;
+                }
+            }
+        }
+
+        for(int i = 0; i < N; i++){
+            System.out.println(arr[i]); //오름차순 정렬된 arr 배열 프린트
+        }
+    }
+}
+
+
+/*public class java2750 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
@@ -16,4 +44,4 @@ public class java2750 {
             System.out.println(arr[a]); //오름차순 정렬된 arr 배열 프린트
         }
     }
-}
+}*/
