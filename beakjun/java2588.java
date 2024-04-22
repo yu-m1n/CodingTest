e44ceb8e7d5b90c5a79bbf16a976240159126ec8
yumin <rdddq01@gmail.com>
2024-04-22 23:20:25 +0900

    coding test

---
+++ beakjun/java2588.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class java2588 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int A = in.nextInt();
+        String B = in.next();
+
+        in.close();
+
+        System.out.println(A * (B.charAt(2) - '0'));
+        System.out.println(A * (B.charAt(1) - '0'));
+        System.out.println(A * (B.charAt(0) - '0'));
+        System.out.println(A * Integer.parseInt(B));
+
+    }
+}
