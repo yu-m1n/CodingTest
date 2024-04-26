a1b97c460d3c00a35f6a4fda159223dacc936d80
yumin <rdddq01@gmail.com>
2024-04-26 19:15:59 +0900

    coding test - unfinished

--- beakjun/java10872.java
+++ beakjun/java10872.java
@@ -1,7 +1,8 @@
-public class java10872 {
-    static int sum(int n, int m){
-        return n * n++;
+public class java10872 { // n! = n * (n - 1) * (n - 2) * ...*1
+    public static void main(String[] args){
+        //int n = 0;
+        System.out.println(factorial(5));
     }
-    //System.out.println(sum(n,m));
+
 }
 
