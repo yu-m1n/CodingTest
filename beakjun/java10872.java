4e5fbddf0b04eb5dcb2a226b30362446304b54a1
yumin <rdddq01@gmail.com>
2024-04-28 23:55:39 +0900

    coding test

--- beakjun/java10872.java
+++ beakjun/java10872.java
@@ -1,8 +1,16 @@
-public class java10872 { // n! = n * (n - 1) * (n - 2) * ...*1
-    public static void main(String[] args){
-        //int n = 0;
-        System.out.println(factorial(5));
+import java.util.Scanner;
+
+public class java10872 {
+    static int fec(int num) {
+        if (num <= 1) return 1; //재귀 종료조건
+        return num * fec(num - 1);
     }
 
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = fec(n);
 
+        System.out.println(sum);
+    }
+}
