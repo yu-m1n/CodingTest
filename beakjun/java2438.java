4428a6fee06bcbfaeecd0f51bde9b3a844f0bd40
yumin <rdddq01@gmail.com>
2024-05-12 23:11:47 +0900

    coding test

--- beakjun/java2438.java
+++ beakjun/java2438.java
@@ -5,7 +5,6 @@ public class java2438 {
         Scanner in = new Scanner(System.in);
 
         int N = in.nextInt();
-        in.close();
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= i; j++) {
