2bbcedc09064574ccae51e238984579e6c6b4c4a
yumin <rdddq01@gmail.com>
2024-06-10 23:41:45 +0900

    Coding test

---
+++ beakjun/java2742.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class java2742 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        in.close();
+
+
+
+        while(N > 0) {
+            System.out.println(N);
+            N--;
+        }
+    }
+}
